b74288e88710aaa4050a001a7e2f58eb935d6ee7
Izaak Dale <izaakdaledev@gmail.com>
2024-04-25 11:23:29 -0700

    add generic cors

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/izaakdale/service-cicd
 
 go 1.22.0
 
-require github.com/izaakdale/ittp v0.1.0
+require (
+	github.com/izaakdale/ittp v0.1.0
+	github.com/rs/cors v1.11.0
+)
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/izaakdale/ittp"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -23,6 +24,15 @@ func main() {
 	if port == "" {
 		port = "80"
 	}
+
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+		AllowedHeaders:   []string{"Authorization", "Content-Type"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	})
+
+	mux.AddMiddleware(cors.Handler)
 
 	log.Printf("up and running\n")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", os.Getenv("HOST"), port), mux))
